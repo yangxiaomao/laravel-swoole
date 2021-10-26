a67dbec460bca4e8ea5133b9b9992fb23cbfc8f3
杨 小毛 <353125014@qq.com>
2021-10-26 12:42:41 +0800

    协程等待组/mysql连接池

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 
-use GuzzleHttp\Promise\Coroutine;
+use App\Pool\mysql\MysqlPool;
+use Swoole\Database\PDOConfig;
+use Swoole\Database\PDOPool;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
+use Swoole\Coroutine\Redis as CoRedis;
+use Swoole\Coroutine\MySQL as CoMysql;
+use Swoole\Coroutine\WaitGroup;
 use Swoole\Runtime;
 use Swoole\Coroutine as Co;
 use function Swoole\Coroutine\run;
@@ -78,8 +84,8 @@ class IndexController extends Controller
             // 发送消息到推送平台
             $pushRes = self::pushMessage();
             if($pushRes){
-                DB::table('user_coupon')->insert($data);
-                echo "发送成功".PHP_EOL;
+                $res = DB::table('user_coupon')->insertGetId($data);
+                Log::info("发送成功，成功ID:".$res);
             }else{
                 echo "发送失败".PHP_EOL;
             }
@@ -104,31 +110,84 @@ class IndexController extends Controller
         Runtime::enableCoroutine();
         //开始记录时间
         $start = microtime(true);
-        run(function() {
-            for($i=0; $i<500; $i++){
-                Co::create(function () {
-                    // redis中获取用户信息，推送消息给用户，用户获取优惠劵
-                    $userInfo = Redis::rpop('swoole_list');
-                    $userArr = json_decode($userInfo, true);
-                    $data = [
-                        'mobile'=>$userArr['mobile'],
-                        'coupon_id'=>$userArr['coupon_id'],
-                        'create_at'=>time()
-                    ];
-                    // 发送消息到推送平台
-                    $pushRes = self::pushMessage();
-                    if($pushRes){
-                        DB::table('user_coupon')->insert($data);
-                        echo "发送成功".PHP_EOL;
-                    }else{
-                        echo "发送失败".PHP_EOL;
-                    }
-                    unset($userInfo);
-                    unset($userArr);
-                    unset($data);
-                });
 
+        run(function() {
+            $pool = new PDOPool((new PDOConfig)
+                ->withHost('127.0.0.1')
+                ->withPort(3306)
+                // ->withUnixSocket('/tmp/mysql.sock')
+                ->withDbName('laravel')
+                ->withCharset('utf8mb4')
+                ->withUsername('root')
+                ->withPassword('123456')
+            );
+            for($j=0; $j <200; $j++){
+                $wg = new WaitGroup();
+                for($i=0; $i<500; $i++){
+                    $wg->add();
+                    Co::create(function () use ($wg, $pool){
+                        $coRedis = new CoRedis();
+                        $coRedis->connect('127.0.0.1', 6379);
+                        // redis中获取用户信息，推送消息给用户，用户获取优惠劵
+                        $userInfo = $coRedis->rpop('laravel_database_swoole_list');
+                        $userArr = json_decode($userInfo, true);
+                        $data = [
+                            'mobile'=>$userArr['mobile'],
+                            'coupon_id'=>$userArr['coupon_id'],
+                            'create_at'=>time()
+                        ];
+                        // 发送消息到推送平台
+                        $pushRes = self::pushMessage();
+                        if($pushRes){
+                            $pdo = $pool->get();
+                            $sql = "INSERT INTO user_coupon ".
+                                "(mobile,coupon_id, create_at) ".
+                                "VALUES ".
+                                "(?,?,?)";
+                            $statement = $pdo->prepare($sql);
+                            if (!$statement) {
+                                throw new \RuntimeException('Prepare failed');
+                            }
+                            if(!isset($userArr['mobile'])){
+                                Log::info("数据异常".$userInfo);
+                            }
+                            $result = $statement->execute([$userArr['mobile'], $userArr['coupon_id'], time()]);
+                            if (!$result) {
+                                throw new \RuntimeException('Execute failed');
+                            }
+                            $pool->put($pdo);
+                            echo "发送成功".PHP_EOL;
+                        }else{
+                            echo "发送失败".PHP_EOL;
+                        }
+                        unset($userInfo);
+                        unset($userArr);
+                        unset($data);
+                        $wg->done();
+                    });
+                    //挂起当前协程，等待所有任务完成后恢复
+
+//                usleep(1000000);
+
+                }
+                $wg->wait();
+//                sleep(2);
             }
+//                $co_status = true;
+//                while($co_status){
+//                    dump(Co::stats());
+//                    $co_status = false;
+//                }
+
+
+//            co::create(function () use ($chan) {
+//                while(1) {
+//                    $data = $chan->pop();
+//
+//                    var_dump($data);
+//                }
+//            });
+//            swoole_event::wait();
         });
 
         //结束时间
